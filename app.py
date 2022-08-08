33e4a9c631cf0bd1c32e37bb357cc2790608cfcc
Alireza Shafi'i <16956112+alirezashafii@users.noreply.github.com>
2022-08-08 20:25:12 +0430

    update app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+# from curses.ascii import isalnum
+
+
+def is_palindrome(teststr):
+    # use the slice trick to reverse the string
+    if teststr == teststr[::-1]:
+        return True
+    return False
+
+
+run = True
+while(run):
+    teststr = input("Enter string to test for palindrome or 'exit':")
+    # if the user types "exit" then quit the program
+    if teststr == "exit":
+        run = False
+        break
+    # convert the string to all lower case
+    teststr = teststr.lower()
+    # strip all the spaces and punctuation from the string
+    newstr = ""
+    for x in teststr:
+        if x.isalnum:
+            newstr += x
+
+    print("Palindrome test:", is_palindrome(newstr))
